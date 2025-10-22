fbe11ed47918e72508aa5b254948c668e6048581
James Westhead <james_westhead@outlook.com>
2025-10-22 22:06:43 +0100

    Update insert.py

--- db/utils/tables/insert.py
+++ db/utils/tables/insert.py
@@ -1,10 +1,13 @@
-def add_till(conn, till):
+def add_till(conn, till_name):
     conn.execute(
         """INSERT INTO tills (till_name)
             SELECT ? WHERE NOT EXISTS(
             SELECT 1 FROM tills WHERE till_name=?);""",
-        [till, till],
+        [till_name, till_name],
     )
+    return conn.execute(
+        "SELECT till_id FROM tills WHERE till_name = ?;", [till_name]
+    ).fetchone()[0]
 
 
 def add_report(conn, month, year, start_date, end_date, till_id, filename):
